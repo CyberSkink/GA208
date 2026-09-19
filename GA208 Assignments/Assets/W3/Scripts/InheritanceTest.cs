ecb4b399063ace529492ab3997e18b90d9ce34c6
CyberSkink <114534445+CyberSkink@users.noreply.github.com>
2026-09-18 17:20:49 -0700

    According to all known laws of aviation

--- GA208 Assignments/Assets/W3/Scripts/InheritanceTest.cs
+++ GA208 Assignments/Assets/W3/Scripts/InheritanceTest.cs
@@ -1,4 +1,4 @@
-/*
+
 using UnityEngine;
 
 public class InheritanceTest : MonoBehaviour
@@ -28,18 +28,32 @@ public class Player
 // don't change anything about the NPC class!
 public class Npc
 {
-	public virtual void Talk(string playerName) {}
+	public virtual void Talk(string playerName) 
+	{
+	playerName = ("According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible.");
+	Debug.Log(playerName);
+	}
 }
 
 // CHANGE THINGS IN VILLAGER...
-public class Villager
+public class Villager : Npc
 {
+	public override void Talk(string playerName)
+	{
+	playerName = ("Hello Player!");
+	Debug.Log(playerName);
+	}
 	
 }
 
 // CHANGE THINGS IN ENEMY...
-public class Enemy
+public class Enemy : Npc
 {
-	
+	public override void Talk(string playerName)
+	{
+	playerName = ("According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible.");
+	Debug.Log(playerName);
+	}
 }
-*/
+
+
